2f44a4aafeb6ec6bb7b02a2de5c5376ba677d61f
Julius Grimm <52246043+Julius-Grimm@users.noreply.github.com>
2021-10-13 23:23:56 +0200

    Update NotionPage.tsx

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -140,7 +140,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
           repo={config.utterancesGitHubRepo}
           issueMap='issue-term'
           issueTerm='title'
-          theme={darkMode.value ? 'photon-dark' : 'github-light'}
+          theme='github-light'
         />
       )
     }
